4ca2f9478252b876cc50d6fdf82f99d3f0363261
Romain Ruaud <romain.ruaud@smile.fr>
2023-01-07 23:54:24 +0100

    test

--- src/Catalog/Model/Catalog.php
+++ src/Catalog/Model/Catalog.php
@@ -1,15 +1,6 @@
 <?php
 /**
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
- * versions in the future.
- *
- * @package   Elasticsuite
- * @author    ElasticSuite Team <elasticsuite@smile.fr>
- * @copyright 2022 Smile
- * @license   Licensed to Smile-SA. All rights reserved. No warranty, explicit or implicit, provided.
- *            Unauthorized copying of this file, via any medium, is strictly prohibited.
+ * z
  */
 
 declare(strict_types=1);
