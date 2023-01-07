9e0c97ac6eb5e01fc59092bfa42dc20a79acd375
Romain Ruaud <romain.ruaud@smile.fr>
2023-01-07 23:26:50 +0100

    Dummy pr for QA

--- src/Catalog/Model/Catalog.php
+++ src/Catalog/Model/Catalog.php
@@ -1,15 +1,6 @@
 <?php
 /**
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Smile ElasticSuite to newer
- * versions in the future.
- *
- * @package   Elasticsuite
- * @author    ElasticSuite Team <elasticsuite@smile.fr>
- * @copyright 2022 Smile
- * @license   Licensed to Smile-SA. All rights reserved. No warranty, explicit or implicit, provided.
- *            Unauthorized copying of this file, via any medium, is strictly prohibited.
+ * wrong
  */
 
 declare(strict_types=1);
@@ -85,7 +76,7 @@ class Catalog
 
     public function getName(): ?string
     {
-        return $this->name;
+        return "nop";
     }
 
     public function setName(string $name): self
