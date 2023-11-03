80ab18e825644f70fa832e135727a8e05dea6bb4
Lorenz HOYET <lorenz.hoyet@smile.fr>
2023-11-03 11:35:47 +0100

    [Search] #add namespacebuilder to opensearch client

--- src/Search/OpenSearch/ClientBuilder.php
+++ src/Search/OpenSearch/ClientBuilder.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Gally\Search\OpenSearch;
 
 use OpenSearch\Namespaces\NamespaceBuilderInterface;
@@ -7,10 +9,12 @@ use OpenSearch\Namespaces\NamespaceBuilderInterface;
 class ClientBuilder extends \OpenSearch\ClientBuilder
 {
     /**
+     * @param array $esConfig
      * @param NamespaceBuilderInterface[] $namespaceBuilder
      */
-    public function __construct(iterable $namespaceBuilders)
+    public function __construct(array $esConfig, iterable $namespaceBuilders)
     {
+        $this->setHosts($esConfig['hosts']);
         foreach ($namespaceBuilders as $namespaceBuilder) {
             $this->registerNamespace($namespaceBuilder);
         }
