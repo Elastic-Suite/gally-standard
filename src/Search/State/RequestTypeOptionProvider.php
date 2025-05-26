1b3ec1182ddae5c6c91e927a19dc7a781c017d1b
Pierre Gauthier <pigau@smile.fr>
2025-05-26 10:59:19 +0200

    [Configuration] #1316095 - Create scope option providers

---
+++ src/Search/State/RequestTypeOptionProvider.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * DISCLAIMER.
+ *
+ * Do not edit or add to this file if you wish to upgrade Gally to newer versions in the future.
+ *
+ * @author    Gally Team <elasticsuite@smile.fr>
+ * @copyright 2022-present Smile
+ * @license   Open Software License v. 3.0 (OSL-3.0)
+ */
+
+declare(strict_types=1);
+
+namespace Gally\Search\State;
+
+use ApiPlatform\Metadata\CollectionOperationInterface;
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use Gally\Search\Elasticsearch\Request\Container\Configuration\ContainerConfigurationProvider;
+
+class RequestTypeOptionProvider implements ProviderInterface
+{
+    public function __construct(
+        private ContainerConfigurationProvider $configurationProvider,
+        private ProviderInterface $itemProvider,
+    ) {
+    }
+
+    /**
+     * @return object|array<int, array{value: string, label: string}>|null
+     */
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
+    {
+        if (!$operation instanceof CollectionOperationInterface) {
+            return $this->itemProvider->provide($operation, $uriVariables, $context);
+        }
+
+        $requestTypes = [];
+        foreach ($this->configurationProvider->getAvailableRequestType('product') as $requestType) {
+            $requestTypes[] = [
+                'value' => $requestType,
+                'label' => $requestType,
+            ];
+        }
+
+        return $requestTypes;
+    }
+}
