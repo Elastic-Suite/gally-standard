73f56bda3a114af0e3933ae161f5d57fbb09c87d
Pierre Gauthier <pigau@smile.fr>
2025-05-26 10:59:19 +0200

    [Configuration] #1316095 - Create scope option providers

---
+++ src/Locale/State/Source/LanguageOptionProvider.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * DISCLAIMER.
+ *
+ * Do not edit or add to this file if you wish to upgrade Gally to newer versions in the future.
+ *
+ * @author    Gally Team <elasticsuite@smile.fr>
+ * @copyright 2022-present Smile
+ * @license   Open Software License v. 3.0 (OSL-3.0)
+ */
+
+declare(strict_types=1);
+
+namespace Gally\Locale\State\Source;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use Gally\Catalog\Repository\LocalizedCatalogRepository;
+use Symfony\Component\Intl\Locales;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class LanguageOptionProvider implements ProviderInterface
+{
+    public function __construct(
+        private LocalizedCatalogRepository $localizedCatalogRepository,
+        private TranslatorInterface $translator,
+    ) {
+    }
+
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): ?array
+    {
+        $usedLocales = array_column($this->localizedCatalogRepository->findUsedLocales(), 'locale');
+
+        return [
+            $this->getGroupOption(
+                'used_language',
+                'gally.language.used.label',
+                $usedLocales
+            ),
+            $this->getGroupOption(
+                'unused_language',
+                'gally.language.unused.label',
+                Locales::getLocales(),
+                $usedLocales
+            ),
+        ];
+    }
+
+    protected function getGroupOption(string $value, string $label, array $locales, array $excludedLocales = []): array
+    {
+        $groupOption['value'] = $groupOption['id'] = $value;
+        $groupOption['label'] = $this->translator->trans($label, [], 'gally_locale');
+
+        $options = [];
+        foreach ($locales as $locale) {
+            // The Regexp allows to verify if the locale has the format xx_XX, locale with the format xx are skipped.
+            if (\in_array($locale, $excludedLocales, true) || 1 !== preg_match('/[a-z]{2}_[A-Z]{2}$/', $locale)) {
+                continue;
+            }
+
+            $language = \Locale::getPrimaryLanguage($locale);
+
+            $option['value'] = $language;
+            $option['label'] = $language;
+
+            $options[$language] = $option;
+        }
+        $groupOption['options'] = array_values($options);
+
+        return $groupOption;
+    }
+}
---
+++ src/Search/Tests/Api/GraphQl/RequestTypeOptionTest.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * DISCLAIMER.
+ *
+ * Do not edit or add to this file if you wish to upgrade Gally to newer versions in the future.
+ *
+ * @author    Gally Team <elasticsuite@smile.fr>
+ * @copyright 2022-present Smile
+ * @license   Open Software License v. 3.0 (OSL-3.0)
+ */
+
+declare(strict_types=1);
+
+namespace Gally\Boost\Tests\Api\GraphQl\Source;
+
+use Gally\Test\AbstractTestCase;
+use Gally\Test\ExpectedResponse;
+use Gally\Test\RequestGraphQlToTest;
+use Gally\User\Constant\Role;
+use Gally\User\Entity\User;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+class RequestTypeOptionTest extends AbstractTestCase
+{
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        // Allows to  load user fixtures.
+        self::loadFixture([]);
+    }
+
+    /**
+     * @dataProvider getCollectionDataProvider
+     */
+    public function testGetCollection(?User $user, array $expectedData, int $responseCode): void
+    {
+        $this->validateApiCall(
+            new RequestGraphQlToTest(
+                <<<GQL
+                    {
+                      requestTypeOptions { value label  }
+                    }
+                GQL,
+                $user,
+            ),
+            new ExpectedResponse(
+                200,
+                function (ResponseInterface $response) use ($expectedData, $responseCode) {
+                    if (200 === $responseCode) {
+                        $this->assertJsonContains(['data' => ['requestTypeOptions' => $expectedData]]);
+                        $boostRequestTypeOptions = $response->toArray()['data']['requestTypeOptions'];
+                        foreach ($boostRequestTypeOptions as $boostRequestTypeOption) {
+                            $this->assertArrayHasKey('label', $boostRequestTypeOption);
+                        }
+                    } else {
+                        $this->assertGraphQlError($expectedData['error']);
+                    }
+                }
+            )
+        );
+    }
+
+    public function getCollectionDataProvider(): array
+    {
+        $requestTypeOptions = [
+            [
+                'value' => 'product_catalog',
+                'label' => 'product_catalog',
+            ],
+            [
+                'value' => 'product_search',
+                'label' => 'product_search',
+            ],
+        ];
+
+        return [
+            [null, ['error' => 'Access Denied.'], 401],
+            [$this->getUser(Role::ROLE_CONTRIBUTOR), $requestTypeOptions, 200],
+            [$this->getUser(Role::ROLE_ADMIN), $requestTypeOptions, 200],
+        ];
+    }
+}
