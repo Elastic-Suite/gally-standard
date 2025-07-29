ec3bd18883d35a9e64a04806dc98795f1c7fd3da
Botis <botis@smile.fr>
2025-07-29 19:12:35 +0200

    draft

--- src/User/Tests/Api/Rest/UserTest.php
+++ src/User/Tests/Api/Rest/UserTest.php
@@ -11,25 +11,22 @@
 
 declare(strict_types=1);
 
-namespace Gally\Catalog\Tests\Api\Rest;
+namespace Gally\User\Tests\Api\Rest;
 
-use Gally\Catalog\Entity\Catalog;
 use Gally\Test\AbstractEntityTestWithUpdate;
 use Gally\User\Constant\Role;
+use Gally\User\Entity\User;
 
 class UserTest extends AbstractEntityTestWithUpdate
 {
     protected static function getFixtureFiles(): array
     {
-        return [
-            __DIR__ . '/../../fixtures/catalogs.yaml',
-            __DIR__ . '/../../fixtures/localized_catalogs.yaml',
-        ];
+        return [];
     }
 
     protected function getEntityClass(): string
     {
-        return Catalog::class;
+        return User::class;
     }
 
     public function createDataProvider(): iterable
@@ -37,77 +34,42 @@ class UserTest extends AbstractEntityTestWithUpdate
         $adminUser = $this->getUser(Role::ROLE_ADMIN);
 
         return [
-            [$adminUser, ['code' => 'valid_code', 'name' => 'B2C Catalog'], 201],
-            [$adminUser, ['code' => 'empty_name', 'name' => ''], 201],
-            [$adminUser, ['code' => 'missing_name'], 201],
-            [null, ['code' => 'valid_code', 'name' => 'JWT Token not found'], 401],
-            [$this->getUser(Role::ROLE_CONTRIBUTOR), ['code' => 'valid_code', 'name' => 'Unauthorized user'], 403],
-            [$adminUser, ['code' => '', 'name' => 'Empty Code'], 422, 'code: This value should not be blank.'],
-            [$adminUser, ['code' => ''], 422, 'code: This value should not be blank.'],
-            [$adminUser, ['name' => 'Missing Code'], 422, 'code: This value should not be blank.'],
+            [$adminUser, ['email' => 'admin@example.com', 'isActive' => true, 'firstName' => 'John', 'lastName' => 'Doe', 'roles' => [Role::ROLE_ADMIN, Role::ROLE_CONTRIBUTOR]], 201],
+            [$adminUser, ['email' => 'contributor@example.com', 'isActive' => true, 'firstName' => 'Alice', 'lastName' => 'Doe', 'roles' => [Role::ROLE_CONTRIBUTOR]], 201],
+            [$adminUser, ['email' => 'admin+bis@example.com', 'isActive' => true, 'firstName' => 'John', 'lastName' => 'Bis', 'roles' => [Role::ROLE_ADMIN, Role::ROLE_CONTRIBUTOR]], 201],
+            [$adminUser, ['email' => 'contributor+bis@example.com', 'isActive' => true, 'firstName' => 'Alice', 'lastName' => 'Bis', 'roles' => [Role::ROLE_CONTRIBUTOR]], 201],
+
+            [$this->getUser(Role::ROLE_CONTRIBUTOR), ['email' => 'forbidden@example.com', 'isActive' => true, 'firstName' => 'Alice', 'lastName' => 'Doe', 'roles' => [Role::ROLE_CONTRIBUTOR]], 403],
+            [null, ['email' => 'jwt@example.com', 'isActive' => true, 'firstName' => 'JWT Token not found', 'lastName' => 'Doe', 'roles' => [Role::ROLE_CONTRIBUTOR]], 401],
+
+            [$adminUser, ['email' => 'contributor@example.com', 'isActive' => true, 'firstName' => 'Alice', 'lastName' => 'Doe', 'roles' => [Role::ROLE_CONTRIBUTOR]], 422, 'email: This email is already associated with another user.'],
+            [$adminUser, [ 'isActive' => true,'firstName' => 'Alice', 'lastName' => 'Doe', 'roles' => [Role::ROLE_CONTRIBUTOR]], 422, 'email: This value should not be blank.'],
+            [$adminUser, ['email' => 'error', 'isActive' => true, 'firstName' => 'Error', 'lastName' => 'Doe', 'roles' => [Role::ROLE_CONTRIBUTOR]], 422, 'email: This value is not a valid email address.'],
+            [$adminUser, ['email' => str_repeat('abcd', 42) . '@examples.com', 'isActive' => true, 'firstName' => 'Error', 'lastName' => 'Doe', 'roles' => [Role::ROLE_CONTRIBUTOR]], 422, 'email: This value is too long. It should have 180 characters or less.'],
+
+            [$adminUser, ['email' => 'error@example.com', 'isActive' => null, 'firstName' => 'Error', 'lastName' => 'Doe', 'roles' => [Role::ROLE_CONTRIBUTOR]], 400, 'The type of the "isActive" attribute must be "bool", "NULL" given.'],
+            [$adminUser, ['email' => 'error@example.com', 'isActive' => '', 'firstName' => 'Error', 'lastName' => 'Doe', 'roles' => [Role::ROLE_CONTRIBUTOR]], 400, 'The type of the "isActive" attribute must be "bool", "string" given.'],
+
+            [$adminUser, ['email' => 'error@example.com', 'isActive' => true, 'lastName' => 'Doe', 'roles' => [Role::ROLE_CONTRIBUTOR]], 422, 'firstName: This value should not be blank.'],
+            [$adminUser, ['email' => 'error@example.com', 'isActive' => true, 'firstName' => '', 'lastName' => 'Doe', 'roles' => [Role::ROLE_CONTRIBUTOR]], 422, 'firstName: This value should not be blank.'],
+
+            [$adminUser, ['email' => 'error@example.com', 'isActive' => true, 'firstName' => 'Error', 'roles' => [Role::ROLE_CONTRIBUTOR]], 422, 'lastName: This value should not be blank.'],
+            [$adminUser, ['email' => 'error@example.com', 'isActive' => true, 'firstName' => 'Error', 'lastName' => '', 'roles' => [Role::ROLE_CONTRIBUTOR]], 422, 'lastName: This value should not be blank.'],
+
+            [$adminUser, ['email' => 'error@example.com', 'isActive' => true, 'firstName' => 'Error', 'lastName' => 'Doe', 'roles' => []], 422, 'roles: This value should not be blank.'],
+            [$adminUser, ['email' => 'error@example.com', 'isActive' => true, 'firstName' => 'Error', 'lastName' => 'Doe', 'roles' => ['ROLE_FAKE']], 422, 'roles: One or more of the given values is invalid.'],
         ];
     }
 
     public function getDataProvider(): iterable
     {
-        $user = $this->getUser(Role::ROLE_CONTRIBUTOR);
+        $user = $this->getUser(Role::ROLE_ADMIN);
 
         return [
-            [$user, 1, ['id' => 1, 'code' => 'b2c_test', 'name' => 'B2C Test Catalog'], 200],
-            [
-                $user,
-                1,
-                [
-                    'id' => 1,
-                    'code' => 'b2c_test',
-                    'name' => 'B2C Test Catalog',
-                    'localizedCatalogs' => [
-                        [
-                            '@id' => $this->getUri('localized_catalogs', '1'),
-                            '@type' => 'LocalizedCatalog',
-                            'id' => 1,
-                            'name' => 'B2C French Store View',
-                            'code' => 'b2c_fr',
-                            'locale' => 'fr_FR',
-                            'isDefault' => false,
-                            'localName' => 'French (France)',
-                        ],
-                        [
-                            '@id' => $this->getUri('localized_catalogs', '2'),
-                            '@type' => 'LocalizedCatalog',
-                            'id' => 2,
-                            'name' => 'B2C English Store View',
-                            'code' => 'b2c_en',
-                            'locale' => 'en_US',
-                            'isDefault' => false,
-                            'localName' => 'English (United States)',
-                        ],
-                    ],
-                ],
-                200,
-                'en_US',
-            ],
-            [
-                $user,
-                1,
-                [
-                    'id' => 1,
-                    'localizedCatalogs' => [
-                        [
-                            'localName' => 'Français (France)',
-                        ],
-                        [
-                            'localName' => 'Anglais (États-Unis)',
-                        ],
-                    ],
-                ],
-                200,
-                'fr_FR',
-            ],
-            [null, 5, ['id' => 5, 'code' => 'missing_name'], 200],
-            [$user, 5, ['id' => 5, 'code' => 'missing_name'], 200],
-            [$this->getUser(Role::ROLE_ADMIN), 5, ['id' => 5, 'code' => 'missing_name'], 200],
-            [$user, 10, [], 404],
+            [null, 3, ['id' => 1, 'email' => 'admin@example.com',], 401],
+            [$this->getUser(Role::ROLE_CONTRIBUTOR), 1, ['id' => 1, 'email' => 'admin@example.com',], 403],
+            [$user, 3, ['id' => 3, 'email' => 'admin@example.com', 'isActive' => true, 'firstName' => 'John', 'lastName' => 'Doe', 'roles' => [Role::ROLE_ADMIN, Role::ROLE_CONTRIBUTOR], 'dummyPassword' =>  '********'], 200],
+            [$user, 7, [], 404],
         ];
     }
 
@@ -118,27 +80,27 @@ class UserTest extends AbstractEntityTestWithUpdate
         return [
             [null, 1, 401],
             [$this->getUser(Role::ROLE_CONTRIBUTOR), 1, 403],
-            [$adminUser, 1, 204],
             [$adminUser, 5, 204],
-            [$adminUser, 10, 404],
+            [$adminUser, 6, 204],
+            [$adminUser, 7, 404],
         ];
     }
 
     public function getCollectionDataProvider(): iterable
     {
         return [
-            [null, 3, 200],
-            [$this->getUser(Role::ROLE_CONTRIBUTOR), 3, 200],
-            [$this->getUser(Role::ROLE_ADMIN), 3, 200],
+            [null, 5, 401],
+            [$this->getUser(Role::ROLE_CONTRIBUTOR), 5, 403],
+            [$this->getUser(Role::ROLE_ADMIN), 4, 200],
         ];
     }
 
     public function patchUpdateDataProvider(): iterable
     {
         return [
-            [null, 1, ['name' => 'B2C Test Catalog PATCH/PUT'], 401],
-            [$this->getUser(Role::ROLE_CONTRIBUTOR), 1, ['name' => 'B2C Test Catalog PATCH/PUT'], 403],
-            [$this->getUser(Role::ROLE_ADMIN), 1, ['name' => 'B2C Test Catalog PATCH/PUT'], 200],
+            [null, 3, ['firstName' => 'John PATCH/PUT'], 401],
+            [$this->getUser(Role::ROLE_CONTRIBUTOR), 3, ['firstName' => 'John PATCH/PUT'], 403],
+            [$this->getUser(Role::ROLE_ADMIN), 3, ['firstName' => 'John PATCH/PUT'], 200],
         ];
     }
 }
