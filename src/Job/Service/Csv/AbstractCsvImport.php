2d16144b4a50cb584067b41bdf4e947087f14bae
Botis <botis@smile.fr>
2025-10-24 11:15:56 +0200

    draft

--- src/Job/Service/Csv/AbstractCsvImport.php
+++ src/Job/Service/Csv/AbstractCsvImport.php
@@ -13,15 +13,21 @@ declare(strict_types=1);
 
 namespace Gally\Job\Service\Csv;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Gally\Catalog\Entity\LocalizedCatalog;
 use Gally\Catalog\Repository\LocalizedCatalogRepository;
 use Gally\Job\Entity\Job;
 use Gally\Job\Service\JobImportInterface;
+use Gally\Thesaurus\Entity\Thesaurus\Expansion;
+use Gally\Thesaurus\Entity\Thesaurus\Synonym;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 abstract class AbstractCsvImport implements JobImportInterface
 {
     public const CODE_SEPARATOR = ';';
+
+    protected ?Job $currentJob = null;
 
     /**
      * @var LocalizedCatalog[]
@@ -30,7 +36,8 @@ abstract class AbstractCsvImport implements JobImportInterface
 
     public function __construct(
         private TranslatorInterface $translator,
-        private array $csvHeadear
+        private KernelInterface $kernel,
+        protected array $csvHeader
     ) {
     }
 
@@ -39,7 +46,7 @@ abstract class AbstractCsvImport implements JobImportInterface
     public function validateImportFile(Job $job): void
     {
         $this->currentJob = $job;
-        $filePath = $job->getImportFile()->contentUrl;
+        $filePath = $this->getAbsoluteFilePath($job->getImportFile()->filePath);
 
         if (!$filePath || !file_exists($filePath)) {
             throw new \InvalidArgumentException(
@@ -63,16 +70,17 @@ abstract class AbstractCsvImport implements JobImportInterface
         try {
             // Validate CSV headers
             $headers = fgetcsv($handle);
-            if (!$headers || array_diff($this->csvHeadear, $headers)) {
+            if (!$headers || array_diff($this->csvHeader, $headers)) {
                 throw new \InvalidArgumentException(
                     $this->translator->trans(
                         'thesaurus.import.error.invalid_headers',
-                        ['expected' => implode(', ', $this->csvHeadear)],
+                        ['expected' => implode(', ', $this->csvHeader)],
                         'gally_thesaurus'
                     )
                 );
             }
 
+            $errors = [];
             $lineNumber = 1;
             while (($data = fgetcsv($handle)) !== false) {
                 $lineNumber++;
@@ -87,13 +95,28 @@ abstract class AbstractCsvImport implements JobImportInterface
                 }
 
                 $associativeData = array_combine($headers, $data);
-                $this->validateCsvLine($associativeData, $lineNumber);
+                $errors = array_merge($errors, $this->validateCsvLine($associativeData, $lineNumber));
+            }
+
+            if (\count($errors) > 0) {
+                throw new \InvalidArgumentException(
+                    $this->translator->trans(
+                        'thesaurus.import.error.validation_failed',
+                        ['line' => $lineNumber, 'errors' => implode(', ', $errors)],
+                        'gally_thesaurus'
+                    )
+                );
             }
 
             $this->logInfo('thesaurus.import.validation.completed');
         } finally {
             fclose($handle);
         }
+    }
+
+    public function getAbsoluteFilePath(string $fileName): string
+    {
+        return $this->kernel->getProjectDir() . '/var/jobs/import/' . ltrim($fileName, '/');
     }
 
     protected function parseScopeCodes(string $scopeCodes): array
@@ -116,38 +139,48 @@ abstract class AbstractCsvImport implements JobImportInterface
         return in_array(strtolower($value), ['1', 'true'], true);
     }
 
-    protected function parseTerms(string $terms, string $type): array
+    protected function parseTerms(string $terms, string $type): ArrayCollection
     {
         if ($type === 'synonym') {
             $groups = explode(';', $terms);
-            $synonymGroups = [];
+            $synonymGroups = new ArrayCollection();
 
             foreach ($groups as $group) {
-                if (strpos($group, ':') !== false) {
-                    [$groupName, $groupTerms] = explode(':', $group, 2);
-                    $synonymGroups[] = [
-                        'name' => trim($groupName),
-                        'terms' => array_map('trim', explode(',', $groupTerms))
-                    ];
-                } else {
-                    $synonymGroups[] = [
-                        'terms' => array_map('trim', explode(',', $group))
-                    ];
+//                if (strpos($group, ':') !== false) {
+//                    [$groupName, $groupTerms] = explode(':', $group, 2);
+//                    $synonymGroups->add([
+//                        'name' => trim($groupName),
+//                        'terms' => new ArrayCollection(array_map('trim', explode(',', $groupTerms)))
+//                    ]);
+//                } else {
+                $synonym = new Synonym();
+                $terms = array_map('trim', explode(',', $group));
+                foreach ($terms as $term) {
+                    $termObject = new Synonym\Term();
+                    $termObject->setTerm(trim($term));
+                    $synonym->addTerm($termObject);
                 }
+                $synonymGroups->add($synonym);
+//                }
             }
 
             return $synonymGroups;
         } else {
-            $expansions = [];
+            $expansions = new ArrayCollection();
             $expansionGroups = explode(';', $terms);
 
             foreach ($expansionGroups as $expansionGroup) {
                 if (strpos($expansionGroup, ':') !== false) {
                     [$referenceTerm, $expansionTerms] = explode(':', $expansionGroup, 2);
-                    $expansions[] = [
-                        'reference_term' => trim($referenceTerm),
-                        'expansion_terms' => array_map('trim', explode(',', $expansionTerms))
-                    ];
+                    $terms = array_map('trim', explode(',', $expansionTerms));
+                    $expansion = new Expansion();
+                    $expansion->setReferenceTerm(trim($referenceTerm));
+                    foreach ($terms as $term) {
+                        $termObject = new Expansion\Term();
+                        $termObject->setTerm(trim($term));
+                        $expansion->addTerm($termObject);
+                    }
+                    $expansions->add($expansion);
                 }
             }
 
@@ -163,14 +196,14 @@ abstract class AbstractCsvImport implements JobImportInterface
         if (empty($this->localizedCatalog)) {
             $localizedCatalogs = $localizedCatalogRepository->findAll();
             foreach ($localizedCatalogs as $localizedCatalog) {
-                $this->localizedCatalog[$localizedCatalog->getCode()] = $localizedCatalog;
+                $this->localizedCatalog[strtolower($localizedCatalog->getCode())] = $localizedCatalog;
             }
         }
 
         return $this->localizedCatalog;
     }
 
-    abstract protected function validateCsvLine(array $data, int $lineNumber): void;
+    abstract protected function validateCsvLine(array $data, int $lineNumber): array;
 
     abstract protected function logInfo(string $message, array $context = []): void;
 
