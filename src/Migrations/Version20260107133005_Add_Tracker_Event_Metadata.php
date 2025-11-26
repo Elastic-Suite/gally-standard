2218d63add82f88bdf20ab1c76794b6d2fca5c12
Pierre Gauthier <pigau@smile.fr>
2025-11-26 18:05:59 +0100

    [Tracker] #1362816 - Add tracker event graphql endpoint

---
+++ src/Migrations/Version20260107133005_Add_Tracker_Event_Metadata.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * DISCLAIMER.
+ *
+ * Do not edit or add to this file if you wish to upgrade Gally to newer versions in the future.
+ *
+ * @author    Gally Team <elasticsuite@smile.fr>
+ * @copyright 2022-present Smile
+ * @license   Open Software License v. 3.0 (OSL-3.0)
+ */
+
+declare(strict_types=1);
+
+namespace Gally\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260107133005_Add_Tracker_Event_Metadata extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add tracker event entity and time series capability';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // Add is_time_series_data in metadata table.
+        $this->addSql('ALTER TABLE metadata ADD is_time_series_data BOOLEAN DEFAULT NULL');
+
+        // Add tracking_event metadata.
+        $this->addSql("INSERT INTO metadata (id, entity, is_time_series_data) VALUES (nextval('metadata_id_seq'), 'tracking_event', true)");
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE metadata DROP is_time_series_data');
+        echo "Skipping metadata deletion. If needed, delete manually with:\n";
+        echo "    DELETE FROM metadata WHERE ...;\n";
+    }
+}
---
+++ src/Tracker/Validator/EventSearchResultValidator.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * DISCLAIMER.
+ *
+ * Do not edit or add to this file if you wish to upgrade Gally to newer versions in the future.
+ *
+ * @author    Gally Team <elasticsuite@smile.fr>
+ * @copyright 2022-present Smile
+ * @license   Open Software License v. 3.0 (OSL-3.0)
+ */
+
+declare(strict_types=1);
+
+namespace Gally\Tracker\Validator;
+
+use Gally\Tracker\Entity\TrackingEvent;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Exception\UnexpectedValueException;
+
+#[\Attribute]
+class EventSearchResultValidator extends AbstractEventValidator
+{
+    public function getEventType(): string
+    {
+        return 'search';
+    }
+
+    public function getMetadataCode(): string
+    {
+        return 'product';
+    }
+
+    protected function checkData(TrackingEvent $event, Constraint $constraint): void
+    {
+        if (!$constraint instanceof EventSearchResult) {
+            throw new UnexpectedValueException($constraint, EventSearchResult::class);
+        }
+
+        if ($event->getEntityCode()) {
+            $this->context
+                ->buildViolation($constraint->entityCodeAtTopLevelMessage)
+                ->atPath('entityCode')
+                ->addViolation();
+        }
+
+        $payload = $this->getPayload($event, $constraint);
+        $this->validateProductListData($payload, $constraint);
+
+        if (!$this->validateDataType($payload, ['search' => 'array'], $constraint)) {
+            return;
+        }
+
+        $this->validateDataType(
+            $payload['search'],
+            ['is_spellchecked' => 'bool', 'query_text' => 'string'],
+            $constraint
+        );
+    }
+}
