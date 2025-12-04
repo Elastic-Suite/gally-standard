5dec54ff428bb08a49e996fe291324957e976be3
Botis <botis@smile.fr>
2025-12-04 17:44:20 +0100

    Fix direct and self deprecation noticies during tests

--- src/Job/Serializer/JobFileNormalizer.php
+++ src/Job/Serializer/JobFileNormalizer.php
@@ -15,7 +15,6 @@ namespace Gally\Job\Serializer;
 
 use Gally\Job\Entity\Job\File;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-use Vich\UploaderBundle\Storage\StorageInterface;
 
 class JobFileNormalizer implements NormalizerInterface
 {
@@ -23,15 +22,12 @@ class JobFileNormalizer implements NormalizerInterface
 
     public function __construct(
         private readonly NormalizerInterface $normalizer,
-        private readonly StorageInterface $storage
     ) {
     }
 
     public function normalize($object, ?string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
     {
         $context[self::ALREADY_CALLED] = true;
-
-        $object->contentUrl = $this->storage->resolveUri($object, 'file');
 
         return $this->normalizer->normalize($object, $format, $context);
     }
