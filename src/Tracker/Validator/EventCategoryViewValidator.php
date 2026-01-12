2464bc316d8f930cee95678017611e145cfcd5fb
Pierre Gauthier <pigau@smile.fr>
2026-01-12 11:03:33 +0100

    [Tracker] #1362816 - Add validators

---
+++ src/Tracker/Validator/EventCategoryViewValidator.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * DISCLAIMER.
+ *
+ * Do not edit or add to this file if you wish to upgrade Gally to newer versions in the future.
+ *
+ * @author    Gally Team <elasticsuite@smile.fr>
+ * @copyright 2022-present Smile
+ * @license   Open Software License v. 3.0 (OSL-3.0)
+ */
+
+declare(strict_types=1);
+
+namespace Gally\Tracker\Validator;
+
+use Gally\Tracker\Entity\TrackingEvent;
+
+#[\Attribute]
+class EventCategoryViewValidator extends AbstractEventValidator
+{
+    public function getEventType(): string
+    {
+        return 'view';
+    }
+
+    public function getMetadataCode(): string
+    {
+        return 'category';
+    }
+
+    protected function checkData(TrackingEvent $event, AbstractEventConstraint $constraint): void
+    {
+        $this->validateEntityCode($event, $constraint);
+        $payload = $this->getPayload($event, $constraint);
+        $this->validateProductListData($payload, $constraint);
+    }
+}
---
+++ src/Tracker/Validator/EventOrderValidator.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * DISCLAIMER.
+ *
+ * Do not edit or add to this file if you wish to upgrade Gally to newer versions in the future.
+ *
+ * @author    Gally Team <elasticsuite@smile.fr>
+ * @copyright 2022-present Smile
+ * @license   Open Software License v. 3.0 (OSL-3.0)
+ */
+
+declare(strict_types=1);
+
+namespace Gally\Tracker\Validator;
+
+use Gally\Tracker\Entity\TrackingEvent;
+
+#[\Attribute]
+class EventOrderValidator extends AbstractEventValidator
+{
+    public function getEventType(): string
+    {
+        return 'order';
+    }
+
+    public function getMetadataCode(): string
+    {
+        return 'product';
+    }
+
+    protected function checkData(TrackingEvent $event, AbstractEventConstraint $constraint): void
+    {
+        $this->validateEntityCode($event, $constraint);
+
+        $payload = $this->getPayload($event, $constraint);
+        if (!$this->validateDataType($payload, ['order' => 'array'], $constraint)) {
+            return;
+        }
+
+        $this->validateDataType(
+            $payload['order'],
+            ['order_id' => 'string', 'total' => 'numeric', 'price' => 'numeric', 'qty' => 'numeric', 'row_total' => 'numeric'],
+            $constraint
+        );
+    }
+}
