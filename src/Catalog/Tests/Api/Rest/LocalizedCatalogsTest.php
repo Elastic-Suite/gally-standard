0c42b652124621bf15696e41f01ef626d26302b8
Pierre Gauthier <pigau@smile.fr>
2026-05-26 18:24:33 +0200

    [Catalog] #1389500 - Make Catalog and LocalizedCatalog name mandatory

--- src/Catalog/Tests/Api/Rest/LocalizedCatalogsTest.php
+++ src/Catalog/Tests/Api/Rest/LocalizedCatalogsTest.php
@@ -40,23 +40,24 @@ class LocalizedCatalogsTest extends AbstractEntityTestWithUpdate
 
         return [
             [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'valid_code', 'name' => 'B2C French catalog', 'locale' => 'fr_FR', 'currency' => 'EUR'], 201],
-            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'empty_name', 'name' => '', 'locale' => 'en_US', 'currency' => 'USD'], 201],
-            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'missing_name', 'locale' => 'fr_FR', 'currency' => 'EUR'], 201],
             [null, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'valid_code', 'name' => 'Unauthorized user', 'locale' => 'fr_FR', 'currency' => 'EUR'], 401],
+            [$this->getUser(Role::ROLE_CONTRIBUTOR), ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'valid_code', 'name' => 'Unauthorized user', 'locale' => 'fr_FR', 'currency' => 'EUR'], 403],
+            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'empty_name', 'name' => '', 'locale' => 'en_US', 'currency' => 'USD'], 422, 'name: This value should not be blank.'],
+            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'missing_name', 'locale' => 'fr_FR', 'currency' => 'EUR'], 422, 'name: This value should not be blank.'],
             [$this->getUser(Role::ROLE_CONTRIBUTOR), ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'valid_code', 'name' => 'Unauthorized user', 'locale' => 'fr_FR', 'currency' => 'EUR'], 403],
             [$adminUser, ['code' => 'no_catalog', 'name' => 'B2C French catalog', 'locale' => 'fr_FR', 'currency' => 'EUR'], 422, 'catalog: This value should not be blank.'],
             [$adminUser, ['catalog' => $this->getUri('catalogs', '2'), 'code' => 'VALID_CODE', 'locale' => 'fr_FR', 'currency' => 'EUR', 'name' => 'Empty Code'], 422, "code: This localized catalog code already exists.\nlocale: This code and locale couple already exists."],
             [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => '', 'locale' => 'en_US', 'currency' => 'USD', 'name' => 'Empty Code'], 422, 'code: This value should not be blank.'],
-            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => '', 'locale' => 'en_US', 'currency' => 'USD'], 422, 'code: This value should not be blank.'],
+            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => '', 'locale' => 'en_US', 'currency' => 'USD'], 422, "code: This value should not be blank.\nname: This value should not be blank."],
             [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'locale' => 'en_US', 'currency' => 'USD', 'name' => 'Missing Code'], 422, 'code: This value should not be blank.'],
-            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'missing_locale', 'currency' => 'EUR'], 422, 'locale: This value should not be blank.'],
-            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'cat_1_invalid', 'locale' => 'fr-fr', 'currency' => 'EUR'], 422, 'locale: This value is not valid.'],
-            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'cat_1_invalid', 'locale' => 'strin', 'currency' => 'EUR'], 422, "locale: This value is not valid.\nlocale: This value is not a valid locale."],
-            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'cat_1_invalid', 'locale' => 'too_long_locale', 'currency' => 'EUR'], 422, "locale: This value is not valid.\nlocale: This value should have exactly 5 characters.\nlocale: This value is not a valid locale."],
-            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'cat_1_invalid', 'locale' => 'a', 'currency' => 'EUR'], 422, "locale: This value is not valid.\nlocale: This value should have exactly 5 characters.\nlocale: This value is not a valid locale."],
-            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'cat_1_invalid', 'locale' => 'abc', 'currency' => 'EUR'], 422, "locale: This value is not valid.\nlocale: This value should have exactly 5 characters.\nlocale: This value is not a valid locale."],
-            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'cat_1_invalid', 'locale' => 'ep_EP', 'currency' => 'EUR'], 422, 'locale: This value is not a valid locale.'],
-            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'cat_1_invalid', 'locale' => 'fr_FR', 'currency' => 'ZZZ'], 422, 'currency: This value is not a valid currency.'],
+            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'missing_locale', 'currency' => 'EUR'], 422, "name: This value should not be blank.\nlocale: This value should not be blank."],
+            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'cat_1_invalid', 'locale' => 'fr-fr', 'currency' => 'EUR'], 422, "name: This value should not be blank.\nlocale: This value is not valid."],
+            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'cat_1_invalid', 'locale' => 'strin', 'currency' => 'EUR'], 422, "name: This value should not be blank.\nlocale: This value is not valid.\nlocale: This value is not a valid locale."],
+            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'cat_1_invalid', 'locale' => 'too_long_locale', 'currency' => 'EUR'], 422, "name: This value should not be blank.\nlocale: This value is not valid.\nlocale: This value should have exactly 5 characters.\nlocale: This value is not a valid locale."],
+            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'cat_1_invalid', 'locale' => 'a', 'currency' => 'EUR'], 422, "name: This value should not be blank.\nlocale: This value is not valid.\nlocale: This value should have exactly 5 characters.\nlocale: This value is not a valid locale."],
+            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'cat_1_invalid', 'locale' => 'abc', 'currency' => 'EUR'], 422, "name: This value should not be blank.\nlocale: This value is not valid.\nlocale: This value should have exactly 5 characters.\nlocale: This value is not a valid locale."],
+            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'cat_1_invalid', 'locale' => 'ep_EP', 'currency' => 'EUR'], 422, "name: This value should not be blank.\nlocale: This value is not a valid locale."],
+            [$adminUser, ['catalog' => $this->getUri('catalogs', '1'), 'code' => 'cat_1_invalid', 'locale' => 'fr_FR', 'currency' => 'ZZZ'], 422, "name: This value should not be blank.\ncurrency: This value is not a valid currency."],
         ];
     }
 
@@ -68,9 +69,6 @@ class LocalizedCatalogsTest extends AbstractEntityTestWithUpdate
             // The first created localized catalog should have been elected as the default one,
             // this is why we test that the parameter isDefault is set to true even if we haven't specified it.
             [$user, 1, ['id' => 1, 'code' => 'b2c_fr', 'locale' => 'fr_FR', 'currency' => 'EUR', 'name' => 'B2C French Store View', 'isDefault' => true, 'localName' => 'Français (France)'], 200, 'fr_FR'],
-            [null, 5, ['id' => 5, 'code' => 'empty_name', 'locale' => 'en_US', 'currency' => 'USD', 'localName' => 'English (United States)', 'isDefault' => false], 200, 'en_US'],
-            [$user, 5, ['id' => 5, 'code' => 'empty_name', 'locale' => 'en_US', 'currency' => 'USD', 'localName' => 'English (United States)', 'isDefault' => false], 200, 'en_US'],
-            [$this->getUser(Role::ROLE_ADMIN), 5, ['id' => 5, 'code' => 'empty_name', 'locale' => 'en_US', 'currency' => 'USD', 'localName' => 'English (United States)', 'isDefault' => false], 200, 'en_US'],
             [$user, 10, [], 404],
         ];
     }
@@ -83,7 +81,6 @@ class LocalizedCatalogsTest extends AbstractEntityTestWithUpdate
             [null, 1, 401],
             [$this->getUser(Role::ROLE_CONTRIBUTOR), 1, 403],
             [$adminUser, 1, 204],
-            [$adminUser, 5, 204],
             [$adminUser, 10, 404],
         ];
     }
@@ -91,9 +88,9 @@ class LocalizedCatalogsTest extends AbstractEntityTestWithUpdate
     public function getCollectionDataProvider(): iterable
     {
         return [
-            [null, 4, 200],
-            [$this->getUser(Role::ROLE_CONTRIBUTOR), 4, 200],
-            [$this->getUser(Role::ROLE_ADMIN), 4, 200],
+            [null, 3, 200],
+            [$this->getUser(Role::ROLE_CONTRIBUTOR), 3, 200],
+            [$this->getUser(Role::ROLE_ADMIN), 3, 200],
         ];
     }
 
